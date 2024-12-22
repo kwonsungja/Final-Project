b2d95bae797b599411f7ccc624ecd5b3965ff5ae
kwonsungja <4gg4g4h4@gmail.com>
2024-12-22 15:29:11 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -6,7 +6,7 @@ st.header("To be a good English teacher")
 # 이미지 추가
 st.subheader("Meet our mascot!")
 # Raw 이미지 링크 사용
-image_url = "https://raw.githubusercontent.com/kwonsungja/KSJ24/main/images/english%20image.png"
+image_url = "https://github.com/kwonsungja/Final-Project/blob/main/images/english%20image.png"
 st.image(image_url, caption="Your friendly mascot!", width=300)
 
 st.write("This cute character is here to cheer you on as you learn and grow! 🌟")
