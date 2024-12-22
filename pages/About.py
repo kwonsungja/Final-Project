5e43c37526111347b7f3180a36b1e4d8309dd29a
kwonsungja <4gg4g4h4@gmail.com>
2024-12-22 15:07:58 +0900

    Create About.py

---
+++ pages/About.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("About Kwon")
+st.header("To be a good English teacher")
+
+# 이미지 추가
+st.subheader("Meet our mascot!")
+# Raw 이미지 링크 사용
+image_url = "https://raw.githubusercontent.com/kwonsungja/KSJ24/main/images/english%20image.png"
+st.image(image_url, caption="Your friendly mascot!", width=300)
+
+st.write("This cute character is here to cheer you on as you learn and grow! 🌟")
