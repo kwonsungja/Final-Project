570954067e7374b529fcdc802f0c500a3a237feb
kwonsungja <4gg4g4h4@gmail.com>
2024-12-22 15:09:06 +0900

    Create About This App.py

---
+++ pages/About This App.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("About This App")
+st.markdown("➰ Hello, Student! :earth_americas:")
+st.markdown("➰ Keep up the good work! :thumbsup: :smile:")
+st.markdown("➰ practice with this app! :tada: :rocket: :heart:")
+st.markdown("➰ URL: ksj24firstapp")
+st.markdown("➰ Since Dec. 17, 2024.")
+
+st.write("To improve about basic rules for forming regular plural. First add -s to the singular noun, Second add -es to nouns ending in -s, -ss, -sh, -ch, -x, -z, or -o. Last if the -y is preceded by a consonant, change -y to -ies. Students sometimes may not know how to use -s, -es, or -ies after singular nouns. By learning these rules, they can resolve noun agreement issues and improve their understanding of nouns. Therefore, by utilizing this app, learners can be encouraged to participate, boosting their motivation and interest in learning English.")
+
+
+st.markdown("""
+- 📝 **Understand Noun rules**  
+- 📚 **Try it repeatedly**  
+- 💪 **Work out for 10 minutes every day**  
+- 🍎 **Remember your learning**  
+- 🌟 **Reflect on today's achievements**  
+""")
